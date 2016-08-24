0ca8ff9309fadcc1d279c3a6af1e504687726202
Eduardo <eduardo.hattori@hotmail.com>
2016-08-23 22:31:42 -0300

    Validação de TOken de autenticação 15 minutos

--- api/modules/domains/tokenDomain.js
+++ api/modules/domains/tokenDomain.js
@@ -7,8 +7,10 @@
 
 module.exports = function(app){
 
-    var client = (app.modules.infra.elasticSearchInfra).getConnection();
-    var moment = require("moment");
+    var client    = (app.modules.infra.elasticSearchInfra).getConnection();
+    var config    = require('config');
+    var moment    = require("moment");
+    var randtoken = require('rand-token');
 
 
     var tokenDomain = {
@@ -23,13 +25,13 @@ module.exports = function(app){
         setToken : function(userid, token, callback){
 
             client.create({
-                index : "usertokens",
-                type : "usertype",
+                index : config.elasticsearch.index,
+                type  : config.elasticsearch.type,
                 id : token,
                 body : {
                     token : token,
                     userid : userid,
-                    data : moment().format("YYYY-MM_DD HH:mm:ss")
+                    data : moment().format("YYYY-MM-DD HH:mm:ss")
                 }
             }, function(err, response){
                 if(err)
@@ -37,8 +39,87 @@ module.exports = function(app){
                 else
                     callback(null,response);
             });
-        }
+        },
 
+        /***
+         * Busca as informações do Token
+         *
+         * @param token
+         * @param callback
+         */
+        getInfoToken : function(token,callback){
+
+            client.getSource({
+                index : config.elasticsearch.index,
+                type  : config.elasticsearch.type,
+                id : token
+            }, function(err, response){
+                if(err) callback(err, null);
+                callback(null, response);
+            });
+        },
+
+        /**
+         * Deleta a informação do token
+         *
+         * @param token
+         * @param callback
+         */
+        deleteToken : function(token, callback){
+
+            client.delete({
+                index : config.elasticsearch.index,
+                type  : config.elasticsearch.type,
+                id : token
+            }, function(err, responde){
+                callback(err,responde);
+            });
+        },
+
+        /***
+         * Cria os tokens
+         *
+         * @param userid
+         * @param callback
+         */
+        createToken : function(userid, callback){
+            this.setToken(userid, randtoken.generate(32), callback);
+        },
+
+
+        isValid : function(token, callback){
+            var _this = this;
+
+            try {
+                this.getInfoToken(token, function(err, res){
+
+                    if(err){
+                        if(err.status)
+                            callback(null, { isValid : false});
+                        else
+                            callback(err);
+                    }
+
+                    var now  = moment();
+                    var then = moment(res.data);
+                    var diff = now.diff(then,"seconds") / 60;
+
+                    if(diff >= 15){
+                        _this.deleteToken(token, function(err, data){
+                            if(err) callback(err);
+                            callback(null, { isValid : false });
+                        });
+                    } else {
+                        callback(null, { isValid : true });
+                    }
+                });
+
+            } catch(e){
+                console.log(e);
+                callback(e);
+            }
+
+        }
     };
 
 
--- api/test/tokenDomainTest.js
+++ api/test/tokenDomainTest.js
@@ -7,48 +7,149 @@
 
 describe("Testa o dominio do token",function(){
 
-
     var app;
     var should;
     var env;
     var tokenDomain;
     var user;
-    var token;
+    var config;
+    var client;
+    var moment;
 
     before(function(done){
 
-        env    = process.env.NODE_ENV;
+        env                  = process.env.NODE_ENV;
+        process.env.NODE_ENV = "test";
+
         app    = require('../app');
         should = require('should');
-        token  = "123";
+        config = require('config');
+        moment = require('moment');
+        client = (app.modules.infra.elasticSearchInfra).getConnection();
 
         tokenDomain = app.modules.domains.tokenDomain;
         user = {
-            userid : 123
+            userid : 123,
+            token  : "123"
         };
 
-        done();
+        client.indices.delete({
+            index: config.elasticsearch.index
+        }, function(err, res) {
+            done();
+        });
     });
 
     it("Deveria setar um token", function(done){
 
-        tokenDomain.setToken(user.userid, token, function(err,data){
-
-            console.log(err,data);
-
+        tokenDomain.setToken(user.userid, user.token, function(err,data){
             if(err) done(err);
-            (data !== undefined).should.be.true();
+            (data._id !== undefined).should.be.true();
             done();
         });
     });
 
     it("Deveria retornar um token a partir de um ID", function(done){
 
+        tokenDomain.getInfoToken(user.token, function(err, data){
+            if(err) done(err);
+            (data.token == user.token).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria gerar um token válido para o user ID", function(done){
+
+        tokenDomain.createToken(user.userid, function(err, data){
+            if(err) done(err);
+            (data._id !== undefined).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria deletar um token", function(done){
+        tokenDomain.deleteToken(user.token, function(err, data){
+           if(err) done(err);
+            try {
+                data.found.should.be.true();
+                done();
+            } catch(e){
+                done(e);
+            }
+        });
+    });
+
+
+    it("Deveria validar retornar token para token válido", function(done){
+
+        var token_valido = "TOKENVALIDO";
+
+        //Cria token valido
+        client.create({
+            index : config.elasticsearch.index,
+            type  : config.elasticsearch.type,
+            id : token_valido,
+            body : {
+                token : token_valido,
+                userid : user.userid,
+                data : moment().format("YYYY-MM-DD HH:mm:ss")
+            }
+        }, function(err, response){
+
+            if(err) done(err);
+            tokenDomain.isValid(token_valido, function(err, data){
+                if(err) done(err);
+                data.isValid.should.be.true();
+                done();
+            });
+        });
+    });
+
+    it("Deveria não validar um token inválido", function(done){
+
+        tokenDomain.isValid("TOKENINVALIDO", function(err, data){
+            if(err) done(err);
+            data.isValid.should.be.false();
+            done();
+        });
+    });
+
+    it("Deveria validar um token expirado como falso", function(done){
+        var token_valido = "OUTROTOKENVALIDO";
+
+        //Cria token valido
+        client.create({
+            index : config.elasticsearch.index,
+            type  : config.elasticsearch.type,
+            id : token_valido,
+            body : {
+                token : token_valido,
+                userid : user.userid,
+                data : moment().subtract(16,'m').format("YYYY-MM-DD HH:mm:ss")
+            }
+        }, function(err, response){
+
+            if(err) done(err);
+            tokenDomain.isValid(token_valido, function(err, data){
+                if(err) done(err);
+                data.isValid.should.be.false();
+                done();
+            });
+        });
+
     });
 
     after(function(done){
-       process.env.NODE_ENV = env;
-        done();
-    });
 
+        client.indices.delete({
+            index: config.elasticsearch.index
+        }, function(err, res) {
+            if (err) {
+                done(err);
+            } else {
+                process.env.NODE_ENV = env;
+                done();
+            }
+        });
+    });
 });
